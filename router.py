b212a9b730464bfcca312bea5ed50042102d43b1
aragaer <aragaer@gmail.com>
2017-08-27 20:58:09 +0300

    Unify endpoints

--- router.py
+++ router.py
@@ -8,10 +8,12 @@ import sys
 
 class Endpoint(object):
 
-    def __init__(self, name, path):
+    def __init__(self, name, path, pack_func, unpack_func):
         self._name = name
         self._path = path
         self._reader = self._writer = None
+        self._unpack = unpack_func
+        self._pack = pack_func
 
     @property
     def name(self):
@@ -24,11 +26,41 @@ class Endpoint(object):
     async def connect(self):
         self._reader, self._writer = await asyncio.open_unix_connection(self._path)
 
-    async def readline(self):
-        return await self._reader.readline()
+    async def read(self):
+        while True:
+            sdata = await self._reader.readline()
+            if not sdata:
+                return None
+            data = sdata.decode().strip()
+            if not data:
+                continue
+            message = self._unpack(data)
+            if message:
+                return message
 
-    def write(self, data):
-        self._writer.write(data.encode())
+    def write(self, message):
+        self.send(self._pack(message))
+
+    def send(self, data):
+        self._writer.write("{}\n".format(data).encode())
+
+
+def unpack_tg_message(data):
+    if data.startswith("message:"):
+        return data[8:].strip()
+
+
+def pack_brain_message(message):
+    if message:
+        return json.dumps({"text":message}, ensure_ascii=False)
+
+
+def unpack_brain_message(data):
+    return json.loads(data)['text']
+
+
+def pack_tg_message(message):
+    return "message: {}".format(message)
 
 
 class Router(object):
@@ -42,38 +74,16 @@ class Router(object):
         e_to = self._endpoints[endpoint_to]
         try:
             while True:
-                sdata = await e_from.readline()
-                if not sdata:
+                message = await e_from.read()
+                if not message:
                     break
-                data = sdata.decode().strip()
-                if not data:
-                    continue
-                if data.startswith("message:"):
-                    message = json.dumps({"text":data[8:]}, ensure_ascii=False)
-                    self._logger.debug("%s -> %s: [%s]", endpoint_from, endpoint_to, message)
-                    e_to.write("{}\n".format(message))
+                self._logger.debug("%s -> %s: [%s]", endpoint_from, endpoint_to, message)
+                e_to.write(message)
         except asyncio.CancelledError:
             pass
 
-    async def listen_brain(self):
-        e_from = self._endpoints['brain']
-        e_to = self._endpoints['tg']
-        try:
-            while True:
-                sdata = await e_from.readline()
-                if not sdata:
-                    break
-                data = sdata.decode().strip()
-                if not data:
-                    continue
-                event = json.loads(data)
-                self._logger.debug("Brain -> TG: [%s]", event['text'])
-                e_to.write("message: {}\n".format(event['text']))
-        except asyncio.CancelledError:
-            pass
-
-    def add_endpoint(self, name, path):
-        self._endpoints[name] = Endpoint(name, path)
+    def add_endpoint(self, name, path, pack, unpack):
+        self._endpoints[name] = Endpoint(name, path, pack, unpack)
 
     def run(self):
         loop = asyncio.get_event_loop()
@@ -83,9 +93,9 @@ class Router(object):
         for endpoint in self._endpoints.values():
             tasks.append(loop.create_task(endpoint.connect()))
         loop.run_until_complete(asyncio.gather(*tasks))
-        self._endpoints['tg'].write('register backend\n')
+        self._endpoints['tg'].send('register backend')
         loop.create_task(self.listen('tg', 'brain'))
-        loop.create_task(self.listen_brain())
+        loop.create_task(self.listen('brain', 'tg'))
         loop.run_forever()
 
 
@@ -94,6 +104,6 @@ if __name__ == '__main__':
     logging.getLogger('router').setLevel(logging.DEBUG)
     logging.getLogger('asyncio').setLevel(logging.WARNING)
     router = Router()
-    router.add_endpoint('tg', "/tmp/pa_socket")
-    router.add_endpoint('brain', "/tmp/pa_brain")
+    router.add_endpoint('tg', "/tmp/pa_socket", pack_tg_message, unpack_tg_message)
+    router.add_endpoint('brain', "/tmp/pa_brain", pack_brain_message, unpack_brain_message)
     router.run()
