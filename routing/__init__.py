fb3127dc7af50db6a70bee022465da607495afa4
aragaer <aragaer@gmail.com>
2017-10-07 18:31:00 +0300

    Rules are attached to faucets.
    Rule gives a name of sink. If "to" is specified it is used as a target
    sink, otherwise faucet rules are processed. Result is NOT added to the
    message.

--- routing/__init__.py
+++ routing/__init__.py
@@ -14,32 +14,55 @@ class Router(object):
 
     def __init__(self, default_sink):
         self._logger = logging.getLogger("router")
-        self._faucets = []
-        self._sinks = []
-        self._sinks.append((Rule(), default_sink))
+        self._faucets = {}
+        self._rules = {}
+        self._sinks = {}
+        self._sinks[None] = default_sink
 
-    def add_sink(self, sink, rule):
-        self._sinks.insert(0, (rule, sink))
+    def add_sink(self, sink, name):
+        self._sinks[name] = sink
 
-    def add_faucet(self, faucet):
-        self._faucets.append(faucet)
+    def add_faucet(self, faucet, name):
+        self._faucets[name] = faucet
+
+    def add_rule(self, rule, faucet_name):
+        if faucet_name not in self._rules:
+            self._rules[faucet_name] = []
+        self._rules[faucet_name].append(rule)
+        self._rules[faucet_name] = sorted(self._rules[faucet_name],
+                                          key=len,
+                                          reverse=True)
 
     def tick(self):
-        for faucet in self._faucets:
+        for faucet_name, faucet in self._faucets.items():
             message = faucet.read()
             if message is None:
                 continue
-            for rule, sink in self._sinks:
-                if rule.matches(message):
-                    sink.write(message)
-                    break
+            dest = message.get("to", None)
+            if dest is None and faucet_name in self._rules:
+                for rule in self._rules[faucet_name]:
+                    if rule.matches(message):
+                        dest = rule.target
+                        break
+            if dest not in self._sinks:
+                dest = None
+            self._sinks[dest].write(message)
 
 
 class Rule(object):
 
-    def __init__(self, **kwargs):
+    def __init__(self, target, **kwargs):
+        self._target = target
         self._clauses = kwargs
+        self._len = len(kwargs)
 
     def matches(self, message):
         source = message['from']
         return all(source.get(k) == v for k, v in self._clauses.items())
+
+    @property
+    def target(self):
+        return self._target
+
+    def __len__(self):
+        return self._len
--- test/router_test.py
+++ test/router_test.py
@@ -34,59 +34,100 @@ class TestFaucet(Faucet):
 class RuleTest(unittest.TestCase):
 
     def test_create(self):
-        rule = Rule(media="test")
+        rule = Rule(target="brain", media="test")
 
         message1 = {"from": {"media": "test"}, "message": "test"}
         message2 = {"from": {"media": "telegram", "user": "123456"}, "message": "test"}
 
         self.assertTrue(rule.matches(message1))
         self.assertFalse(rule.matches(message2))
+        self.assertEqual(rule.target, "brain")
+
+    def test_len(self):
+        self.assertEqual(len(Rule(target="", media="test")), 1)
 
 
 class RouterTest(unittest.TestCase):
 
     _router = None
     _sink = None
+    _faucet = None
 
     def setUp(self):
         self._sink = TestSink()
         self._router = Router(default_sink=self._sink)
+        self._faucet = TestFaucet()
+        self._router.add_faucet(self._faucet, name="test")
 
     def test_tick(self):
         self._router.tick()
 
-    def test_add_faucet(self):
-        faucet = TestFaucet()
+    def test_default_sink(self):
         message = {"from": {"media": "test"}, "message": "test"}
 
-        self._router.add_faucet(faucet)
-        faucet.create_message(message)
+        self._faucet.create_message(message)
 
         self._router.tick()
 
         self.assertEquals(self._sink.messages, [message])
 
-    def test_add_sink(self):
-        rule = Rule(media="telegram", user="123456")
-        sink = TestSink()
+    def test_no_message(self):
         faucet = TestFaucet()
+
+        self._router.add_faucet(faucet, name="empty")
+
+        self._router.tick()
+        # most important is that this doesn't raise any exceptions
+
+        self.assertEquals(self._sink.messages, [])
+
+    def test_add_sink(self):
+        rule = Rule(target="sink", media="telegram", user="123456")
+        sink = TestSink()
         message1 = {"from": {"media": "test"}, "message": "test"}
         message2 = {"from": {"media": "telegram", "user": "123456"}, "message": "test"}
         message3 = {"from": {"media": "telegram", "user": "1234567"}, "message": "test"}
+        message4 = {"from": {"media": "test"}, "to": "sink", "message": "test"}
 
-        self._router.add_sink(sink, rule)
+        self._router.add_sink(sink, name="sink")
 
-        self._router.add_faucet(faucet)
-        for message in (message1, message2, message3):
-            faucet.create_message(message)
+        self._router.add_rule(rule, faucet_name="test")
+        for message in (message1, message2, message3, message4):
+            self._faucet.create_message(message)
             self._router.tick()
 
         self.assertEqual(self._sink.messages, [message1, message3])
-        self.assertEqual(sink.messages, [message2])
+        self.assertEqual(sink.messages, [message2, message4])
 
-    def test_no_message(self):
-        self._router.add_faucet(TestFaucet())
+    def test_message_with_to_but_sink_not_present(self):
+        message = {"from": {"media": "test"}, "to": "inexistent", "message": "test"}
 
+        self._faucet.create_message(message)
         self._router.tick()
 
-        self.assertEqual(self._sink.messages, [])
+        self.assertEqual(self._sink.messages, [message])
+
+    def test_more_specific_rule_has_priority(self):
+        rule1 = Rule(target="sink1", media="telegram", user="123456")
+        rule2 = Rule(target="sink2", media="telegram")
+        rule3 = Rule(target="sink3", media="telegram", user="654321")
+
+        for rule in (rule1, rule2, rule3):
+            self._router.add_rule(rule, faucet_name="test")
+
+        sinks = []
+        for name in ("sink1", "sink2", "sink3"):
+            sinks.append(TestSink())
+            self._router.add_sink(sinks[-1], name=name)
+
+        message1 = {"from": {"media": "telegram", "user": "123456"}, "message": "test"}
+        message2 = {"from": {"media": "telegram", "user": "000000"}, "message": "test"}
+        message3 = {"from": {"media": "telegram", "user": "654321"}, "message": "test"}
+
+        for message in (message1, message2, message3):
+            self._faucet.create_message(message)
+            self._router.tick()
+
+        self.assertEqual(sinks[0].messages, [message1])
+        self.assertEqual(sinks[1].messages, [message2])
+        self.assertEqual(sinks[2].messages, [message3])
